9bb1a4ddeecbb17014b3eb7fdd6dd55a8201e039
k1llbyte <killbyte.prototype@gmail.com>
2022-06-27 05:10:25 +0100

    Portfolio style

---
+++ portfolio/layouts/default.vue
@@ -0,0 +1,90 @@
+<template>
+    <v-app dark flat>
+
+        <v-app-bar
+        :clipped-left="clipped"
+        fixed
+        app
+        height="70">
+            <!--
+            <v-row justify="center" align="center">
+            <v-col cols="16" sm="10" md="7">
+            -->
+                <!--
+                <v-btn
+                icon
+                @click.stop="clipped = !clipped">
+                    <v-icon>md-github</v-icon>
+                </v-btn>
+                -->
+                <v-spacer/>
+
+                <v-toolbar-title class="text-uppercase">
+                    <span>Jorge</span>
+                    <span class="font-weight-light">Mota</span>
+                </v-toolbar-title>
+
+                <v-tabs
+                v-model="tab"
+                right
+                dark
+                color="orange"
+                icons-and-text>
+
+                    <v-tab
+                    background-color="orange"
+                    href="/projects">
+                        Projects
+                        <v-icon>mdi-format-list-bulleted</v-icon>
+                    </v-tab>
+
+                    <v-tab
+                    background-color="orange"
+                    href="/about-me">
+                        About Me
+                        <v-icon>mdi-account</v-icon>
+                    </v-tab>
+                </v-tabs>
+            <!--
+            </v-col>
+            </v-row>
+            -->
+        </v-app-bar>
+        
+        <v-main>
+        <v-container>
+            <Nuxt />
+        </v-container>
+        </v-main>
+
+    </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Jorge Mota'
+    }
+  }
+}
+</script>
---
+++ portfolio/pages/projects.vue
@@ -0,0 +1,80 @@
+<template>
+    <v-row justify="center" align="center">
+        <v-col cols="16" sm="10" md="7">
+            <v-row v-for="item in items" justify="center">
+                <!-- Full Card -->
+                <v-card class="ma-2 mx-auto">
+                    <v-container>
+                        <v-row no-gutters>
+                            <!-- Left Card -->
+                            <v-col
+                            cols="12"
+                            sm="5">
+                                <v-card>
+                                    <v-img
+                                    class="white--text align-end"
+                                    height="250px"
+                                    :src="item.img">
+                                        <v-card-title>{{item.title}}</v-card-title>
+                                    </v-img>
+                                </v-card>
+                            </v-col>
+                            <!-- Right Card -->
+                            <v-col
+                            cols="12"
+                            sm="7">
+                                <v-card-actions >
+                                    <v-btn
+                                    color="orange"
+                                    block
+                                    text
+                                    :href="item.redirect">
+                                        Github
+                                    </v-btn>
+                                </v-card-actions>
+                                <v-card-text class="text--primary">
+                                    <div>
+                                        Whitehaven Beach<br/>
+                                        <b>Lorem</b> ipsum dolor sit amet consectetur adipisicing elit. Incidunt fuga, perspiciatis nam sed nisi rerum mollitia sunt! Aliquid asperiores sequi dolorum temporibus nesciunt impedit et hic excepturi natus accusamus.
+                                    </div>
+                                </v-card-text>
+                            </v-col>
+                        </v-row>
+                    </v-container>
+                </v-card>
+            </v-row>
+        </v-col>
+    </v-row>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+        items: [
+            { 
+                title: 'Kazan Engine',
+                img: 'https://camo.githubusercontent.com/f5265753dd313a60b82005dffcb1c9503fa819a0bd8cb226807a45b3b61eb3a2/68747470733a2f2f692e696d6775722e636f6d2f6d6954386a36742e706e67',
+                redirect: 'https://github.com/K1llByte/kazan'
+            },
+            { 
+                title: 'Ray Tracer',
+                img: 'https://raw.githubusercontent.com/K1llByte/cool_rt/master/images/preview2.png',
+                redirect: 'https://github.com/K1llByte/cool_rt'
+            },
+            { 
+                title: 'Efficient FFT',
+                img: 'https://i.imgur.com/nMRuKkl.png',
+                redirect: 'https://github.com/K1llByte/master_thesis'
+            },
+            { 
+                title: 'OpenGL Renderer',
+                img: 'https://i.imgur.com/gXyBuIH.png',
+                redirect: 'https://github.com/K1llByte/cg_project'
+            }
+        ]
+    }
+  }
+}
+</script>
